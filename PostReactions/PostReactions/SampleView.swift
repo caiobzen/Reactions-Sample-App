727d6222f4fabd33a99d156f238e6ec8c09e06d4
Carlos Corrêa <caiobzen@gmail.com>
2019-09-27 20:53:37 -0300

    feat: add Ractions Swift Package Module

--- PostReactions/PostReactions/SampleView.swift
+++ PostReactions/PostReactions/SampleView.swift
@@ -1,13 +1,14 @@
 import SwiftUI
+import Reactions
 
 struct SampleView: View {
     
     let reactions = [
-        React(name: "hand.thumbsup.fill", color: Color(red: 0, green: 0.8, blue: 1)),
-        React(name: "smiley.fill", color: .yellow),
-        React(name: "flame.fill", color: .red),
-        React(name: "star.fill", color: .yellow),
-        React(name: "heart.fill", color: Color(red: 1, green: 0.4, blue: 0.3)),
+        Reaction(name: "hand.thumbsup.fill", color: Color(red: 0, green: 0.8, blue: 1)),
+        Reaction(name: "smiley.fill", color: .yellow),
+        Reaction(name: "flame.fill", color: .red),
+        Reaction(name: "star.fill", color: .yellow),
+        Reaction(name: "heart.fill", color: Color(red: 1, green: 0.4, blue: 0.3)),
     ]
     
     var body: some View {
@@ -16,7 +17,9 @@ struct SampleView: View {
             .font(Font.custom("American Lemon", size: 90.0))
             .foregroundColor(.black)
             
-            ReactionsView(reactions: reactions)
+            Reactions(reactions: reactions) { reaction in
+                print("selected reaction \(reaction)")
+            }
         }
         .frame(minWidth: .zero, maxWidth: .infinity, minHeight: .zero, maxHeight: .infinity)
         .background(Color(red: 1, green: 1, blue: 0.92))
